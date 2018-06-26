9649504c4a38587c53ecd043f915b5a95069fd56
nkstamilan <37336232+nkstamilan@users.noreply.github.com>
2018-06-26 12:05:42 +0530

    Update prime between 2 nos.c

--- C Programming/beginner/prime between 2 nos.c
+++ C Programming/beginner/prime between 2 nos.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-
-int main(void) {
-  int n,i,n1,j;
-  printf("enter the start number");
-  scanf("%d",&n);
-  printf("enter the ending number");
-  scanf("%d",&n1);
-  for(i=n;i<=n1;i++)
-  {
-    for(j=2;j<n;j++)
+#include<stdio.h>
+int main()
+{
+    int n,n1,i,fact,j;
+    printf("Enter the Number");
+    scanf("%d",&n);
+    printf("Enter the Number");
+    scanf("%d",&n1);
+    printf("Prime Numbers are: \n");
+    for(i=n; i<=n1; i++)
     {
-    if(i%j==0)
-      continue;
-      
-    else
-    {
-      printf("%d",i);
-      return 0;
-  }
+        fact=0;
+        for(j=2; j<=n1; j++)
+        {
+            if(i%j==0)
+                fact++;
+        }
+        if(fact==1)
+            printf("%d " ,i);
     }
-  }
-  return 0;
+return 0;
 }
